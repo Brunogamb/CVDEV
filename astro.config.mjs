aacdda27d6910a769db28c341c61cd7425e0d215
Brunogamb <brunogamberini24@gmail.com>
2026-04-19 01:19:31 -0300

    Update site URL in astro.config.mjs to reflect new GitHub Pages path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://brunogamb.github.io/',
+    site: 'https://brunogamb.github.io/CVDEV/',
     base: "/CVDEV/"
 });
