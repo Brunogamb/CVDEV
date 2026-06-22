5898e2f78b13d1a0d9b3879c36a830f7be5fed6f
Brunogamb <brunogamberini24@gmail.com>
2026-06-22 17:21:05 -0300

    Remove GitHub Pages deployment workflow and update site configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://brunogamb.github.io/',
-    base: "/CVDEV"
+    site: 'https://brunogamb.com.ar/',
+    base: "/cv"
 });
